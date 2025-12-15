8ea6195f5efae56e3e1427b1a9d62d4d3e9950df
GKExpo <theshardulkamble@gmail.com>
2025-12-15 16:48:45 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -1,29 +1,15 @@
 import path from 'path';
-import { defineConfig, loadEnv } from 'vite';
+import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
-export default defineConfig(({ mode }) => {
-  const env = loadEnv(mode, '.', '');
+export default defineConfig({
+  base: '/SmartSpend/', // REQUIRED for GitHub Pages
 
-  return {
-    base: '/SmartSpend/', // ✅ REQUIRED for GitHub Pages
+  plugins: [react()],
 
-    server: {
-      port: 3000,
-      host: '0.0.0.0',
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, '.'),
     },
-
-    plugins: [react()],
-
-    define: {
-      'process.env.API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-      'process.env.GEMINI_API_KEY': JSON.stringify(env.GEMINI_API_KEY),
-    },
-
-    resolve: {
-      alias: {
-        '@': path.resolve(__dirname, '.'),
-      },
-    },
-  };
+  },
 });
